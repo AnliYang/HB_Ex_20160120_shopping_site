aed9bd342a5228931e101f6cd904d7f9ff1ebf06
Hackbright Student <info@hackbrightacademy.com>
2016-01-20 18:43:36 -0800

    implemented add to cart function, need to finish cart.html

--- shoppingsite.py
+++ shoppingsite.py
@@ -7,7 +7,7 @@ Authors: Joel Burton, Christian Fernandez, Meggie Mahnken.
 """
 
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, session
 import jinja2
 
 import melons
@@ -50,7 +50,7 @@ def show_melon(melon_id):
     Show all info about a melon. Also, provide a button to buy that melon.
     """
 
-    melon = melons.get_by_id(59)
+    melon = melons.get_by_id(melon_id)
     print melon
     return render_template("melon_details.html",
                            display_melon=melon)
@@ -71,6 +71,7 @@ def shopping_cart():
     #   - keep track of the total amt of the entire order
     # - hand to the template the total order cost and the list of melon types
 
+    # pass session/cart info
     return render_template("cart.html")
 
 
@@ -88,8 +89,28 @@ def add_to_cart(id):
     #
     # - add the id of the melon they bought to the cart in the session
 
-    return "Oops! This needs to be implemented!"
+    # if user has no cart yet:
+    #     start a session
+    #     create a cart
 
+    if 'cart' in session:
+        session['cart'].append(id)
+    else:
+        session['cart'] = [id]
+    
+    # if the melon (by melon_id) is already in the cart:
+    #     add one more
+    # else:
+    #     add the melon (by melon_id) to the cart with qty 1
+
+    # flash message: You added a melon! Yay
+    cart = session['cart']
+
+    melon_dict = melons.read_melon_types_from_file("melons.txt")
+
+    print melon_dict
+
+    return render_template("cart.html", cart=cart, melons=melon_dict)
 
 @app.route("/login", methods=["GET"])
 def show_login():
